fa22809586d754639cfe8f1a89501f334651128f
Hilll19 <hilworking0110@gmail.com>
2023-07-31 15:07:42 +0700

    fix code container text

--- components/About.jsx
+++ components/About.jsx
@@ -6,12 +6,12 @@ const About = () => {
     <div className="w-full md:h-screen items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols3 gap-8">
         <div className="col-span-2">
-          <p className="mt-10 uppercase text-xl tracking-widest text-[#308C8A]">
+          <p className="mt-16 ml-2 uppercase text-xl font-bold tracking-widest text-[#308C8A]">
             About
           </p>
-          <h2 className="py-3">Hello World</h2>
-          <p className="p-2 text-gray-600">//Programmer</p>
-          <p className="p-2 text-gray-600">
+          <h2 className="py-3 ml-2">Hello World</h2>
+          <p className="p-2 text-gray-600 ml-2">//Programmer</p>
+          <p className="p-2 text-gray-600 ml-2">
             Mochamad Hilmy Febrian 20 years old, a dedicated and enthusiastic
             web developer. Have a solid foundation in HTML, CSS and JavaScript.
             With 2 years of experience in this field during college student,
@@ -19,7 +19,7 @@ const About = () => {
             projects.Understand responsive design principles and strive to
             create seamless experiences across devices and browsers
           </p>
-          <p className="p-2 text-gray-600">
+          <p className="p-2 text-gray-600 ml-2">
             Have skilled in using modern frameworks and libraries like React, to
             build dynamic and interactive interfaces.Always try to stay up to
             date with the latest trends and technologies to further my knowledge
@@ -32,13 +32,13 @@ const About = () => {
           <div className="flex items-center justify-between mb-5 xl:mb-[-20px]">
             <button
               href=""
-              className="bg-gray-400 hover:bg-blue-700 text-white font-bold py-2 px-3 ml-[130px] rounded-full cursor-pointer hover:scale-105 ease-in duration-300 xl:ml-[575px]"
+              className="bg-[#308C8A] hover:bg-[#306b6a] text-white font-bold py-2 px-3 ml-[130px] rounded-full cursor-pointer hover:scale-105 ease-in duration-300 xl:ml-[575px]"
             >
               Download
             </button>
           </div>
         </div>
-        <div className="xl:w-auto h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-2 hover:scale-105 ease-in duration-300 xl:ml-[530px]">
+        <div className="xl:w-auto w-[200px] ml-[94px] h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-2 hover:scale-105 ease-in duration-300 xl:ml-[530px]">
           <Image src="/../public/assets/hilll.jpeg" width="200" height="60" />
         </div>
       </div>
