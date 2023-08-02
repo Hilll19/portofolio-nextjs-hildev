7e82cffa483af31fe934b5a2ae7d36eda340da20
Hilll19 <hilworking0110@gmail.com>
2023-08-02 23:16:43 +0700

    Update change projectUrl, add page moreinfo

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -20,21 +20,21 @@ const Projects = () => {
             title="Kopi Emak"
             backgroundImg={propertyImg}
             descriptionText="HTML CSS JAVASCRIPT Basic"
-            projectUrl="/kopiemak"
+            projectUrl="/property"
           />
           {/* Rembang */}
           <Projectitem
             title="Rembang"
             backgroundImg={rembangImg}
             descriptionText="HTML CSS JAVASCRIPT Basic"
-            projectUrl="/rembang"
+            projectUrl="/property"
           />
           {/* ChatApp */}
           <Projectitem
             title="ChatApp Firebase"
             backgroundImg={chatappImg}
             descriptionText="ReactJS Firebase"
-            projectUrl="/chatapp"
+            projectUrl="/property"
           />
         </div>
       </div>
